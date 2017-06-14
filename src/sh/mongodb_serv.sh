d7e75efad8ba162c6b5378fc42fbab2290392971
Salam Hiyali <hiyali920@gmail.com>
2017-06-14 17:43:42 +0800

    removed run sh from docker

--- src/sh/mongodb_serv.sh
+++ src/sh/mongodb_serv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # foreground
-# mongod --dbpath $(PWD)/db --port 27188
+mongod --dbpath $(PWD)/db --port 27188
 
 # background
-mongod --fork --dbpath /hunt/db --port 27188 --logpath /hunt/log/mongod.log
-sleep 5
+# mongod --fork --dbpath /hunt/db --port 27188 --logpath /hunt/log/mongod.log
+# sleep 5
 
