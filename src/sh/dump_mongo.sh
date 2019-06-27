24631e3f1cf70738cfe5bcac70e117d2e9315ecd
hiyali <hiyali920@gmail.com>
2019-06-27 04:32:31 +0000

    Changed dump_mongo.sh

--- src/sh/dump_mongo.sh
+++ src/sh/dump_mongo.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 DATEVAR=$(date +%y-%m-%d_%H)
-CRAWL_NUM=1
-if [ $1 ]; then
-  CRAWL_NUM=$1
-fi
 
 mongodump --db youtubeMusic -o ./ && tar -zcvf ytMs-$DATEVAR.tar.gz youtubeMusic && rm -rf youtubeMusic
 
 # move to ~/pmm/dl to use.
+# restore:
+# tar -xvzf community_images.tar.gz
+# mongorestore --host <database-host> -d <database-name> --port <database-port> foldername
