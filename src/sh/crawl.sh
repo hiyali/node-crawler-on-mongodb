1d9b9bd9702305e5afc52e553b8bd0ac683236dc
Salam Hiyali <hiyali920@gmail.com>
2019-07-31 13:34:06 +0000

    Update comments for cron job timing

--- src/sh/crawl.sh
+++ src/sh/crawl.sh
@@ -4,4 +4,4 @@ DATEVAR=$(date +%Y-%m-%d_%H-%M-%S)
 ./node_modules/.bin/babel-node ./src/crawl.js 2>&1 >> ./logs/crawl_$DATEVAR.log # --dev-mode
 
 # crontab -e # (Not require sudo)
-# 23 9 * * * cd pmm/crawler && ./src/sh/crawl.sh
+# 8 4 * * * cd pmm/crawler && ./src/sh/crawl.sh
